b82c89c36b0f7f2231f7f40222acf479a4852a4a
Yasminpontess <161251647+Yasminpontess@users.noreply.github.com>
2025-01-27 09:49:31 -0300

    Inicialização do projeto

---
+++ src/main/java/com/aplicativofit/FarmaciaBemEstarApplication.java
@@ -0,0 +1,13 @@
+package com.aplicativofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaBemEstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaBemEstarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplicativofit/FarmaciaBemEstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicativofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaBemEstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
